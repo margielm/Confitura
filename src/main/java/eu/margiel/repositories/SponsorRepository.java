485cce144ed6fae6dc1c5ae45673967d76bc6226
lukaszlenart <lukasz.lenart@gmail.com>
2011-05-17 08:23:06 +0200

    Adds sorting option for Partners base on money they've spent

--- src/main/java/eu/margiel/repositories/SponsorRepository.java
+++ src/main/java/eu/margiel/repositories/SponsorRepository.java
@@ -1,13 +1,14 @@
 package eu.margiel.repositories;
 
-import java.util.List;
-
-import org.synyx.hades.dao.GenericDao;
-
 import eu.margiel.domain.Sponsor;
+import org.synyx.hades.dao.GenericDao;
+import org.synyx.hades.dao.Query;
+
+import java.util.List;
 
 public interface SponsorRepository extends GenericDao<Sponsor, Integer> {
 
+    @Query("from Sponsor s where s.type = ?1 order by money desc")
 	List<Sponsor> readByType(String type);
 
 }
