cca6a2eaff163acb773bd228690dd57c9f34df80
lukaszlenart <lukasz.lenart@gmail.com>
2011-05-19 07:00:45 +0200

    Adds Participants registration counter

--- src/main/java/eu/margiel/pages/javarsovia/BaseWebPage.java
+++ src/main/java/eu/margiel/pages/javarsovia/BaseWebPage.java
@@ -9,11 +9,16 @@ import eu.margiel.pages.javarsovia.news.NewsFeed;
 import eu.margiel.pages.javarsovia.sponsor.SponsorWidget;
 import eu.margiel.repositories.MenuRepository;
 import eu.margiel.repositories.NewsRepository;
+import eu.margiel.repositories.ParticipantRepository;
 import org.apache.wicket.Resource;
 import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.rome.FeedResource;
+
+import java.io.Serializable;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static eu.margiel.domain.SponsorType.MEDIA;
@@ -26,6 +31,8 @@ public class BaseWebPage extends JavarsoviaBasePage {
 	private MenuLinks menuItemList;
 	@SpringBean
 	private NewsRepository newsRepository;
+    @SpringBean
+    private ParticipantRepository participantRepository;
 
 	private WebMarkupContainer wrapper;
 
@@ -47,6 +54,7 @@ public class BaseWebPage extends JavarsoviaBasePage {
 		add(new MainMenuPanel("mainMenu", menuRepository.getMainMenu(), menuItemList));
 		add(new SponsorWidget("sponsors", SponsorType.sponsors()));
 		add(new SponsorWidget("media", newArrayList(MEDIA)));
+        add(new Label("registrationCounter", new Model<Serializable>(participantRepository.count())));
 		//add(new SpeakersWidget("speakers"));
 	}
 
